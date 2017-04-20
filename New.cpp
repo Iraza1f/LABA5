34db7c53a1dda0f86ec6b9d29b0fa784ab0a2834
Dmitriy Kuznetsov <dmitriykuznetsow@gmail.com>
2017-04-21 01:22:29 +0700

    Осталось только вывод в файл

--- New.cpp
+++ New.cpp
@@ -3,7 +3,6 @@
 #include <locale.h>
 #include <fstream> 
 #include <iostream> 
-#include <string>
 #include <conio.h>
 #include <queue>
 using namespace std;
@@ -12,104 +11,152 @@ int main()
 	queue<int> que1;
 	queue<int> que2;
 	queue<int> que3;
-	setlocale(LC_ALL, "Russian");
-	cout << "Нажмите n/ 1. чтобы внести элемент в первую очередь n/ 2. чтобы внести элемент во вторую очередь n/ 3. чтобы вытолкнуть элемент из первой очереди n/ 4. чтобы вытолкнуть элемент из второй очереди n/ 5. чтобы показать элементы первой очереди n/ 6. чтобы показать элементы второй очереди n/ 7. чтобы показать элементы очередей n/ 8. слияние " << endl;
 	int p1 = 0;
 	int p2 = 0;
 	int variant = 0;
-	int s = 1;
-	switch (variant) {
-	case 1:
-		while (s != 0)
-		{ 
-			s = 1;
+	int x = 0;
+	int s = 0;
+	setlocale(LC_ALL, "Russian");
+	while (s == 0)
+	{
+		cout << "Нажмите \n 1. чтобы внести элемент в первую очередь \n 2. чтобы внести элемент во вторую очередь \n 3. чтобы вытолкнуть элемент из первой очереди \n 4. чтобы вытолкнуть элемент из второй очереди \n 5. чтобы показать элементы первой очереди \n 6. чтобы показать элементы второй очереди \n 7. слияние \n 8. выход \n 9. для вывода очередей " << endl;
+		cin >> variant;
+		switch (variant)
+		{
+		case 1:
+			system("cls");
 			cout << "Введите число" << endl;
 			cin >> p1;
 			que1.push(p1);
-			cout << "n\ Для выхода нажмите Escape, для ввода ещё одного числа Enter" << endl;
-			int i_input;
-			i_input = _getch();
-			switch (i_input)
-			{
-			case 27:
-				s = 0;
-			}
-		}
-		break;
-	case 2:
-		while (s != 0)
-		{
-			s = 1;
+			break;
+
+		case 2:
+			system("cls");
 			cout << "Введите число" << endl;
 			cin >> p2;
 			que2.push(p2);
-			cout << "n\ Для выхода нажмите Escape, для ввода ещё одного числа Enter" << endl;
-			int i_input;
-			i_input = _getch();
-			switch (i_input)
+			break;
+
+		case 3:
+			system("cls");
+			if (que1.empty() == true)
 			{
-			case 27:
-				s = 0;
+				cout << "Очередь пуста" << endl;
 			}
-		}
+			else
+			{
+					que1.pop();
+			}
+			break;
+
+		case 4:
+			system("cls");
+			if (que2.empty() == true)
+			{
+				cout << "Очередь пуста" << endl;
+			}
+			else
+			{
+					que2.pop();
+			}
+			break;
+		case 5:
+			system("cls");
+			if (que1.empty() == true)
+			{
+				cout << "Первая очередь пуста" << endl;
+			}
+			else
+			{	
+				cout << "Элементы первой очереди" << endl;
+				que3 = que1;
+				while (!que3.empty())
+				{
+					cout << que3.front() << " ";
+					que3.pop();
+				}
+				cout << endl;
+			}
+			break;
+		case 6:
+			system("cls");
+			if (que2.empty() == true)
+			{
+			cout << "Вторая очередь пуста" << endl;
+			}
+			else
+			{
+			que3 = que2;
+			cout << "Элементы второй очереди" << endl;
+			while (!que3.empty())
+			{
+				cout << que3.front() << " ";
+				que3.pop();
+			}
+			cout << endl;
 		break;
-	case 3:
-		while (s != 0)
-		{
+		case 7:
+			cout << "Сколько элементов убрать?" << endl;
+			cin >> x;
+			if (x < que1.size())
+			{
+
+				for (int i = 0; i < x; i++)
+					que1.pop();
+				while (!que2.empty())
+				{
+					que1.push(que2.front());
+					que2.pop();
+				}
+			}
+			else
+				cout << "Ошибка" << endl;
+			break;
+
+		case 8:
 			s = 1;
-			que1.pop();
-			cout << "n\ Для выхода нажмите Escape, для выталкивания ещё одного числа Enter" << endl;
-			int i_input;
-			i_input = _getch();
-			switch (i_input)
+			break;
+
+		case 9:
+			system("cls");
+			if (que1.empty() == true)
 			{
-			case 27:
-				s = 0;
+				cout << "Первая очередь пуста" << endl;
 			}
-		}
-		break;
-	case 4:
-		while (s != 0)
-		{
-			s = 1;
-			que2.pop();
-			cout << "n\ Для выхода нажмите Escape, для выталкивания ещё одного числа Enter" << endl;
-			int i_input;
-			i_input = _getch();
-			switch (i_input)
+			else
 			{
-			case 27:
-				s = 0;
+				cout << "Элементы первой очереди" << endl;
+				que3 = que1;
+				while (!que3.empty())
+				{
+					cout << que3.front() << " ";
+					que3.pop();
+				}
+				cout << endl;
 			}
-		}
-		break;
-	case 5:
-		que3 = que1;
-		while (!que3.empty())
+			if (que2.empty() == true)
+			{
+				cout << "Вторая очередь пуста" << endl;
+			}
+			else
+			{
+				cout << "Элементы второй очереди" << endl;
+				que3 = que2;
+				while (!que3.empty())
+				{
+					cout << que3.front() << " ";
+					que3.pop();
+				}
+				cout << endl;
+			}
+			break;
+		default:
 		{
-			cout << que3.front() << " ";
-			que3.pop();
+				   cout << "Ошибка" << endl;
+				   break;
 		}
-		break;
-	case 6:
-		que3 = que2;
-		while (!que3.empty())
-		{
-			cout << que3.front() << " ";
-			que3.pop();
 		}
-		break;
-	case 7:
-		break;
-	case 8:
-		break;
-	default:
-	{
-			   cout << "Выход из программы..." << endl;
-			   exit(EXIT_SUCCESS);
-			   break;
+		}
 	}
-	}
-	
-	cout << ("Программа завершена. Результат сохранен") << endl;
+	cout<< "Программа завершена. Результат сохранен" << endl;
 }
